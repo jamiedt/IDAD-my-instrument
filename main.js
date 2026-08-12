6dd3a14c02e51db601d5da650c12cdebe7a4b64f
jamiedt <s4092802@student.rmit.edu.au>
2026-08-12 14:49:14 +1000

    added dialog
    added dialog with button to close it

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-// find testt button
+// browser loads html > browser loads javascript > open the dialog > user closes dialog > audio system loads > user clicks sound button
+// find dialog and close button
+const introDialog = document.getElementById("intro-dialog");
+const introDialogClose = document.getElementById("intro-dialog-close");
+
+// find test button
 const testButton = document.getElementById("test-button");
 
 // create synth
 const synth = new Tone.Synth().toDestination();
+
+////// DIALOG //////
+// open dialog
+introDialog.showModal();
+
+// add event listener to the dialog close button
+introDialogClose.addEventListener("click", function closeIntroDialog() {
+  introDialog.close();
+});
 
 // add event listener to the button
 testButton.addEventListener("click", playNote);
